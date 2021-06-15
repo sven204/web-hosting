227f9817030f08112d410ebeb132af3829ac6dea
Michal Koziel <michkoz26@gmail.com>
2021-06-15 17:51:30 +0100

    made it work

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
->>> def run(server_class=HTTPServer, handler_class=BaseHTTPRequestHandler):
-...     server_address = ('', 8000)                                        
-...     httpd = server_class(server_address, handler_class)   
-...     httpd.serve_forever()
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+def run(server_class=HTTPServer, handler_class=BaseHTTPRequestHandler):
+    server_address = ('', 8000)                                        
+    httpd = server_class(server_address, handler_class)   
+    httpd.serve_forever()
+
+run()
